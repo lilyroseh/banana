722776d9bbbc155e7762530cd40ba26899aa3309
Tim Fuchs <criseone@me.com>
2022-11-23 22:02:32 +0100

    added glTF functionality (but broken atm)

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -4,11 +4,11 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/script.js'),
+    entry: path.resolve(__dirname, './src/main.js'),
     output:
     {
         filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, './dist')
     },
     devtool: 'source-map',
     plugins:
@@ -19,7 +19,7 @@ module.exports = {
             ]
         }),
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
+            template: path.resolve(__dirname, '../index.html'),
             minify: true
         }),
         new MiniCSSExtractPlugin()
